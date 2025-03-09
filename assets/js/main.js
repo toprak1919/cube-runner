ed7cf843e8db6a0dd6389c8c7d7d7de46d9c6a8a
toprak1919 <toprakdikici28@hotmail.com>
2025-03-09 03:32:03 +0100

    Add placeholder main.js in assets/js to handle incorrect references

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+// This is a placeholder file to handle incorrect references
+// Redirect to the correct main.js file
+console.warn('Incorrect path accessed: /assets/js/main.js. Use /js/main.js instead.');
+window.location.href = '../index.html';
